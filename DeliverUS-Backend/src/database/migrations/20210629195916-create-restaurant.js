9e76bf79c543cd573d9690fca0fe09c9b354f8cc
AngSanRui <angsanrui2@alum.us.es>
2024-02-23 10:17:22 +0100

    trabajo de laboratorio

--- DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
+++ DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
@@ -6,8 +6,76 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
-      }
+      },
       // TODO: Include the rest of the fields of the Restaurants table
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      postalCode: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      url: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      shippingCosts: {
+        allowNull: false,
+        type: Sequelize.DOUBLE
+      },
+      email: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      phone: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      logo: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      heroImage: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      status: {
+        allowNull: true,
+        type: Sequelize.ENUM,
+        values: [
+          'online',
+          'offline',
+          'closed',
+          'temporarily closed'
+        ]
+      },
+      restaurantCategoryId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'RestaurantCategories'
+          },
+          key: 'id'
+        }
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'Users'
+          },
+          key: 'id'
+        }
+      }
 
     })
   },
